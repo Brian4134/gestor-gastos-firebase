53ca9df217ed5a59ae6cc3026c06508745303013
Brian <infinitoran6@gmail.com>
2025-12-03 11:28:30 -0500

    SECURITY: Remove exposed credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,3 @@
-// Configuración de Firebase para Vercel
-export const firebaseConfig = {
-  type: "service_account",
-  project_id: "registroalumno-d7870",
-  private_key_id: "bc52d9499768277e0e0f4c9d87c3209dc955c96d",
-  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC7BD6i5cuQ8MSk\nxfYNxijw9tL/J6Y7BadzAi55vI1ke9w+T9+ewTouqVcL6qV6IN5lJ09TMQrtmnUi\nvaps34WFEiOmgUrhwWMZedGbYlTXxFOl5VnyKzGESVeUQ0NOCoW4j+COfzqFx4/+\ncDWwaLJm8Vdc80D/2QF5L9967X/uQHGHlNyw7fdP0WQHvwvbCZqyF33YyUFeqxbU\nktIT9jyeSmufmh0Y6ZFcWXtuxqXlhGVOZk0cGWbSZnIy8L91nJBLMvTwGgTavOty\n1APmqSFrxnj2qoP+ls4GVnn0sAP3mweh/s3XmgZEPZWGAqvASPKDwyRuDmyZqdDC\nOvtzM81zAgMBAAECggEANkBTeUbnKCsoKFKC/9jpvIkliT4zwzBvNHZADDGN5MXn\njK34BWIxYiLerQbLLSQGz9o48T2rWWTysPrLSx0GayVpcR5VnirO+C92Bjjeut7p\nkxj24ED2ctUh0lE235c+1lzqe/vderx7AYkEkiG1CxbczIqGYxHNEx43mr313COX\nuMxdzLitZ6ZCnySslGjWNhmq44Fw+08t830e5n59pXTMie5v3bG4Z5scVjEZevf2\n7126umGhUMc8R82T3Fz4U4+n1AnBYSMmuh50B1sdtjQAYX5urIaKxcE20GGbtjvG\nFPqBZhOv2LIajoJtI6/AOETa5qtZIVMVfnAeAMWagQKBgQDa1wAogOPndD/3BLmX\nH+anCp61oW0jMdsW0LzzBS+uOYQ5J9SisMLGj0eT5WbS5UGtNDnkMQPhkzFHjz5u\nf6+PTxLAXV7iDBrdx7gl+zX0Ti/SgAvgBG0DHtdYIvfRYztrJGFmAmQGDG2rs1FX\nP0qsqKn95+NJULTn6lUYZPOM8wKBgQDaxeHVRyxj/9lGNlB0vJJdLX050pfBw7LI\nd6bKYvauHoFLSB+6FWXewmKEQgV0Z5v9fg7/9a7v+diVX5woEQ3sra0EFMeuIvC4\nitmInj5KGIk5VrczDI+dbVuP3XE7oyU2uLUKDRCYOavLoAI3LmB/m/iHJwZAb3+2\n93rbYATdgQKBgDjt/5uJ1EANDOrc7s7yvJUt/RWxFew29xDbYpwx+DJxwgUAaJ+e\nLj1rXqqJ3hL1Ev3U/U5wtdrD6Oj9ybcDPo+geXdTkvBpR7/0SeKPlHymrmgraWtw\nm09ZgZnrFt8A2p9HypK4An9UUl99eaahz3JVOkvUPFuBRF6kplh/QqCjAoGAcoP8\nd4ie+uVU1UTfDowazT1ffGH44AtRTVAcdXoiB/id00s4mE5VGaqEqRhqBImrhKB0\nt5Th8mnE7kOy2OI8eLRciXNbJpLRkLfVsIasslpvkh0LHmSn1E8HC6JtgDsg4qkC\n0vUNx7EAdUEXh8z15DY/fHd39UioAJ1AqQ503YECgYEAoTJ4x/QuyHeXxdNwBeZK\nSQs9QSwR7cudwZENsxaVUmVJMED7634GYxcUJl9u8PXS5ssXE4HKCHVmEx9ykMIu\nF1JfU7XsCrzhEpxAUsnjZ77gW+rl68jozyb/b/klETZNeWZLT2AcMR9yoGHy9xib\nXs6hSsoQyyDV7Pd6zR/n9JM=\n-----END PRIVATE KEY-----\n",
-  client_email: "firebase-adminsdk-fbsvc@registroalumno-d7870.iam.gserviceaccount.com",
-  client_id: "117159856386358362273",
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40registroalumno-d7870.iam.gserviceaccount.com",
-  universe_domain: "googleapis.com"
-};
+// Este archivo no debe contener credenciales
+// Las credenciales se configuran en variables de entorno de Vercel
+console.log('Firebase config cargado desde variables de entorno');
